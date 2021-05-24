a1773e30dd5085c6724fb7c20f25fb0e375e405a
Robson Ferreira <tech.rob@icloud.com>
2021-05-24 15:48:17 -0400

    added routes to app

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const router = express.Router()
 
 const path = __dirname + '/views/'
 
+
+router.use((req, res, next) => {
+console.log(`'/' ${req.method}`)
+})
+
+router.get('/', (req, res) => res.sendFile(path + 'index.html'))
+
+router.get('/heroes', (req, res) => res.sendFile(path + 'heroes.html'))
+
+app.use(express.static(path))
+app.use('/', router)
+
+
+
+
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Server listening at localhost:${port}`))
 
