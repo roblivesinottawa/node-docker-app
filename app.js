e7ecc0f6dbc4efdf44f3e237cd9c1274032b95b6
Robson Ferreira <tech.rob@icloud.com>
2021-05-25 14:07:11 -0400

    added html and css

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-const express = require('express')
-const app = express()
-const router = express.Router()
+const express = require("express");
+const app = express();
+const router = express.Router();
 
-const path = __dirname + '/views/'
-
+const path = __dirname + "/views/";
 
 router.use((req, res, next) => {
-console.log(`'/' ${req.method}`)
-})
+  console.log(`'/' ${req.method}`);
+});
 
-router.get('/', (req, res) => res.sendFile(path + 'index.html'))
+router.get("/", (req, res) => res.sendFile(path + "index.html"));
 
-router.get('/heroes', (req, res) => res.sendFile(path + 'heroes.html'))
+router.get("/python", (req, res) => res.sendFile(path + "python.html"));
 
-app.use(express.static(path))
-app.use('/', router)
+app.use(express.static(path));
+app.use("/", router);
 
-
-
-
-const port = process.env.PORT || 4000
-app.listen(port, () => console.log(`Server listening at localhost:${port}`))
-
-
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Server listening at localhost:${port}`));
